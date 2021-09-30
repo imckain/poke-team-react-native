eed43f43250db436a7b69d3b6d324348054842a3
imckain <ianmmckain@gmail.com>
2021-09-30 00:13:44 -0400

    Navigation is working properly, routes are working as expected, bottom tab navigation is functional- added ternary for icon changes in tab navigation

--- App.js
+++ App.js
@@ -1,13 +1,98 @@
+import * as React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+import { FontAwesome, FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import HomeScreen from './src/screens/HomeScreen';
+import TeamsScreen from './src/screens/TeamsScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function BottomTabNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let ioniconName;
+          let materialIconName;
+          let fontAwesomeName;
+
+          if (route.name === 'Home') {
+            ioniconName = focused ? 'ios-home' : 'home-outline'
+            return <Ionicons name={ioniconName} size={24} color="black" />
+          } else if (route.name === 'Teams') {
+            materialIconName = focused ? 'pokeball' : 'pokemon-go'
+            return <MaterialCommunityIcons name={materialIconName} size={24} color="black" />
+          } else if (route.name === 'Profile') {
+            fontAwesomeName = focused ? 'user-circle' : 'user-circle-o'
+            return <FontAwesome name={fontAwesomeName} size={24} color="black" />
+          }
+        }
+      })}
+    >
+      <Tab.Group
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#272537',
+          },
+          headerTitleStyle: {
+            color: '#ffffff',
+            fontWeight: '600',
+          },
+        }}
+        >
+        <Tab.Screen 
+          name='Home' 
+          component={HomeScreen} 
+          options={{
+            title: 'Home'
+          }}
+          />
+        <Tab.Screen 
+          name='Teams' 
+          component={TeamsScreen} 
+          options={{
+            title: 'Your Teams'
+          }}
+          />
+        <Tab.Screen 
+          name='Profile' 
+          component={ProfileScreen} 
+        />
+      </Tab.Group>
+    </Tab.Navigator>
+  )
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName='Tab Navigator' 
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#272537',
+          },
+          headerTitleStyle: {
+            color: '#ffffff',
+            fontWeight: '600',
+          },
+        }}
+        options={{
+          title: 'Poke-Team'
+        }}
+      >
+        <Stack.Group>
+          <Stack.Screen options={{ title: 'Poke-Team' }} name='Tab Navigator' component={BottomTabNavigator} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,4 +103,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backButton: {
+		// marginLeft: 18,
+		// marginBottom: 10,
+		// marginTop: 5,
+	},
 });
+
+export default App;
