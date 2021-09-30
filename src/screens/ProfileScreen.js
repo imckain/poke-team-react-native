cd5194e99a00e065a2cf9fc3921b09c5348f22c7
imckain <ianmmckain@gmail.com>
2021-09-30 00:11:18 -0400

    profile screen

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+
+const ProfileScreen = () => {
+
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default React.memo(ProfileScreen);
