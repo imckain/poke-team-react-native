0550986d1c99745050a35f011c4b7ff530807b58
imckain <ianmmckain@gmail.com>
2021-09-30 20:12:19 -0400

    moved logo into a sub folder for workflow

---
+++ src/components/header/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+
+const Logo = () => {
+
+  return (
+    <View style={styles.logoContainer}>
+      <Text style={styles.logoText}>Poke Team</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  logoContainer: {
+  },
+  logoText: {
+    fontSize: 32,
+    fontWeight: '600',
+    color: '#fff',
+    alignSelf: 'flex-start',
+  }
+});
+
+export default Logo;
