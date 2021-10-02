60d32e2bc53e777e45892a5798f2b8ee79697c1c
imckain <ianmmckain@gmail.com>
2021-10-02 11:58:15 -0400

    refactored- placed code block sections into their own components for flie structure/readability

--- src/screens/DetailModal.js
+++ src/screens/DetailModal.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+
+import PokemonNameAndId from '../components/pokemonDetailComponents/PokemonNameAndId';
+import FrontSprite from '../components/pokemonDetailComponents/FrontSprite';
+import BackSprite from '../components/pokemonDetailComponents/BackSprite';
+import BaseStats from '../components/pokemonDetailComponents/BaseStats';
+import TypeDetail from '../components/pokemonDetailComponents/TypeDetail';
 
 const DetailModal = (props) => {
   const results = props.route.params.results[0]
@@ -11,41 +17,15 @@ const DetailModal = (props) => {
   return (
     <View style={styles.container}>
       <View style={styles.mainInfo}>
-        <Text style={styles.name}>{Capitalize(results.name)} #{results.id}</Text> 
+        <PokemonNameAndId fontSize={42} results={results} />
         <View style={styles.spriteContainer}>
-          <Image style={styles.imageStyle} source={{ uri: results.sprites.front_default }} />
-          <Image style={styles.imageStyle} source={{ uri: results.sprites.back_default }} />
+          <FrontSprite width={200} height={200} results={results} />
+          <BackSprite width={200} height={200} results={results} />
         </View>
       </View>
       <View style={styles.detailInfo}>
-        <View>
-          <Text style={styles.typeLabelText}>Type:  <Text style={styles.typeText}>{results.types.map(el => Capitalize(el.type.name) + ' ')}</Text></Text>
-        </View>
-        <Text style={styles.detailHeaderText}>Base Stats:</Text>
-        <View style={styles.baseStatContainer}>
-          <Text style={styles.detailStatNameText}>{results.stats[0].stat.name}:</Text>
-          <Text style={styles.detailBaseStatText}>{results.stats[0].base_stat}</Text>
-        </View>
-        <View style={styles.baseStatContainer}>
-          <Text style={styles.detailStatNameText}>{results.stats[1].stat.name}:</Text>
-          <Text style={styles.detailBaseStatText}>{results.stats[1].base_stat}</Text>
-        </View>
-        <View style={styles.baseStatContainer}>
-          <Text style={styles.detailStatNameText}>{results.stats[2].stat.name}:</Text>
-          <Text style={styles.detailBaseStatText}>{results.stats[2].base_stat}</Text>
-        </View>
-        <View style={styles.baseStatContainer}>
-          <Text style={styles.detailStatNameText}>{results.stats[3].stat.name}:</Text>
-          <Text style={styles.detailBaseStatText}>{results.stats[3].base_stat}</Text>
-        </View>
-        <View style={styles.baseStatContainer}>
-          <Text style={styles.detailStatNameText}>{results.stats[4].stat.name}:</Text>
-          <Text style={styles.detailBaseStatText}>{results.stats[4].base_stat}</Text>
-        </View>
-        <View style={styles.baseStatContainer}>
-          <Text style={styles.detailStatNameText}>{results.stats[5].stat.name}:</Text>
-          <Text style={styles.detailBaseStatText}>{results.stats[5].base_stat}</Text>
-        </View>
+        <TypeDetail margin={13} headerFontSize={32} detailFontSize={32} results={results} />
+        <BaseStats headerFontSize={32} detailFontSize={18} results={results} />
       </View>
     </View>
   );
@@ -54,12 +34,12 @@ const DetailModal = (props) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-    borderColor: '#fff',
-    borderWidth: 2,
     width: '100%',
     height: '100%',
     alignSelf: 'center',
-    backgroundColor: '#272537',
+    backgroundColor: '#464450',
+    paddingHorizontal: 10,
+
   },
   spriteContainer: {
     flexDirection: 'row'
@@ -70,60 +50,11 @@ const styles = StyleSheet.create({
     maxWidth: '100%',
     paddingTop: 20
   },
-  name: {
-    textAlign: 'center',
-    color: '#fff',
-    fontSize: 32,
-    fontWeight: '600',
-    marginVertical: 8,
-    marginHorizontal: 12
-  },
   detailInfo: {
     flex: 1,
-    borderColor: '#ff0000',
-    borderWidth: 2,
     paddingLeft: 5,
     paddingVertical: 8,
   },
-  typeLabelText: {
-    color: '#fff',
-    fontSize: 22,
-    marginBottom: 10,
-    fontWeight: '600'
-  },
-  typeText: {
-    color: '#fff',
-    fontSize: 18,
-    marginBottom: 10
-  },
-  baseStatContainer: {
-    flexDirection: 'row',
-    paddingLeft: 10,
-    paddingRight: 6,
-    justifyContent: 'space-between',
-    width: '100%',
-    paddingVertical: 2,
-  },
-  detailHeaderText: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: '600'
-  },
-  detailStatNameText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  detailBaseStatText: {
-    color: '#fff',
-    alignSelf: 'flex-end',
-    fontWeight: '600',
-    fontSize: 16,
-  },
-  imageStyle: {
-    height: 150,
-    width: 150,
-    marginBottom: 16,
-  }
 });
 
 export default React.memo(DetailModal);
