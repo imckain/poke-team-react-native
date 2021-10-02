86b7abb6f68153fc7693c7c23b48e0804c1d7222
imckain <ianmmckain@gmail.com>
2021-10-02 11:55:01 -0400

    added nav buttons, subject to change

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,31 +1,55 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, Keyboard, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import AdvancedSearchNavigator from '../components/navigatorCards/AdvancedSearchNavigator';
+
+import BuildTeamsNavigator from '../components/navigatorCards/BuildTeamsNavigator';
+import ProfileNavigator from '../components/navigatorCards/ProfileNavigator';
 import SearchBar from '../components/navigatorCards/SearchBar';
+import TeamsNavigator from '../components/navigatorCards/ViewTeamsNavigator';
 import ShowSearchResult from '../components/teamBuilder/ShowSearchResult';
 
 import useResults from '../hooks/useResults';
+
+
+const HideKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
 
 const HomeScreen = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchAPI, results] = useResults([]);
 
   return (
-    <View style={styles.container}>
-      <SearchBar 
-        searchTerm={searchTerm} 
-        onSearchTermChange={setSearchTerm} 
-        onSearchTermSubmit={() => searchAPI(searchTerm)}
-      />
-      <Pressable onPress={() => props.navigation.navigate('Detail Modal', { results: results })}>
-        <ShowSearchResult results={results} />
-      </Pressable>
-      <Pressable style={styles.buttonStyle} onPress={() => props.navigation.navigate('Teams')} >
-        <Text>Teams</Text>
-      </Pressable>
-      <Pressable style={styles.buttonStyle} onPress={() => props.navigation.navigate('Profile')} >
-        <Text>Profile</Text>
-      </Pressable>
-    </View>
+    <HideKeyboard>
+      <ScrollView style={styles.container}>
+        <SearchBar 
+          searchTerm={searchTerm} 
+          onSearchTermChange={setSearchTerm} 
+          onSearchTermSubmit={() => searchAPI(searchTerm)}
+        />
+        <Pressable onPress={() => props.navigation.navigate('Detail Modal', { results: results })}>
+          <ShowSearchResult results={results} />
+        </Pressable>
+        <View style={styles.teamsNavContainer}>
+          <Pressable style={styles.buttonStyle} onPress={() => props.navigation.navigate('Teams')} >
+            <TeamsNavigator />
+          </Pressable>
+          <Pressable style={styles.buttonStyle} onPress={() => props.navigation.navigate('Build Teams')}>
+            <BuildTeamsNavigator />
+          </Pressable>
+        </View>
+        <View style={styles.largeNavContainer}>
+          <Pressable style={styles.largeButtonStyle} onPress={() => props.navigation.navigate('Search', { results: results })}>
+            <AdvancedSearchNavigator results={results} />
+          </Pressable>
+          <Pressable style={styles.largeButtonStyle} onPress={() => props.navigation.navigate('Profile')} >
+            <ProfileNavigator />
+          </Pressable>
+        </View>
+      </ScrollView>
+    </HideKeyboard>
   );
 };
 
@@ -37,9 +61,32 @@ const styles = StyleSheet.create({
   buttonStyle: {
     fontSize: 32,
     fontWeight: '600',
-    marginVertical: 15,
     textAlign: 'center',
     alignSelf: 'center'
+  },
+  largeButtonStyle: {
+    fontSize: 32,
+    width: '100%',
+    fontWeight: '600',
+    textAlign: 'center',
+    alignSelf: 'center',
+    marginBottom: 1
+  },
+  teamsNavContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 20,
+    justifyContent: 'space-evenly',
+    backgroundColor: '#464450',
+    borderRadius: 10,
+    width: '90%'
+  },
+  largeNavContainer: {
+    width: '90%',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    marginTop: 20,
   },
 });
 
