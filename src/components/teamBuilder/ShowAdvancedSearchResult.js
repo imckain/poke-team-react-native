0e27d7c218e9c3e73881428a4f0fe2d75c9e1a4f
imckain <ianmmckain@gmail.com>
2021-10-02 12:00:37 -0400

    Advanced search result card

---
+++ src/components/teamBuilder/ShowAdvancedSearchResult.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import BaseStats from '../pokemonDetailComponents/BaseStats';
+import TypeDetail from '../pokemonDetailComponents/TypeDetail';
+import PokemonNameAndId from '../pokemonDetailComponents/PokemonNameAndId';
+import FrontSprite from '../pokemonDetailComponents/FrontSprite';
+
+const ShowAdvancedSearchResult = (props) => {
+  if (!props.results.length) {
+    return null;
+  }
+
+  const results = props.results[0]
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <View style={styles.mainInfo}>
+          <PokemonNameAndId fontSize={32} results={results} />
+          <FrontSprite width={150} height={150} results={results} />
+        </View>
+        <View style={styles.detailInfo}>
+          <BaseStats headerFontSize={22} detailFontSize={16} results={results} />
+          <TypeDetail margin={7} headerFontSize={22} detailFontSize={16} results={results} />
+        </View>
+      </View>
+      <Text style={styles.infoNotice}>Tap for more info</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderRadius: 10,
+    width: '90%',
+    height: 'auto',
+    alignSelf: 'center',
+    backgroundColor: '#464450',
+  },
+  mainInfo: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    maxWidth: '60%'
+  },
+  detailInfo: {
+    flex: 1,
+    paddingHorizontal: 5,
+    paddingVertical: 8,
+  },
+  infoNotice: {
+    textAlign: 'center',
+    color: '#ffffffa7',
+    fontStyle: 'italic',
+    paddingTop: 2,
+  }
+});
+
+export default React.memo(ShowAdvancedSearchResult);
