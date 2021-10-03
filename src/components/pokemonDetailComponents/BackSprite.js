29512a89d3e020da54b46dfe88bc5908d781305f
imckain <ianmmckain@gmail.com>
2021-10-02 23:59:40 -0400

    pulling in animated sprites

--- src/components/pokemonDetailComponents/BackSprite.js
+++ src/components/pokemonDetailComponents/BackSprite.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
 
 const BackSprite = ({ results, width, height }) => {
+  const sprite = (el) => {
+    if (el.sprites.versions["generation-v"]["black-white"].animated.back_default === null) {
+      return el.sprites.back_default
+    } else {
+      return el.sprites.versions["generation-v"]["black-white"].animated.back_default
+    }
+  }
 
   return (
-    <Image style={[styles.imageStyle, { width: width, height: height }]} source={{ uri: results.sprites.back_default }} />
+    <Image 
+      style={[styles.imageStyle, { width: width, height: height }]} 
+      resizeMode='contain' 
+      source={{ uri: sprite(results) }} 
+    />
   );
 };
 
--- src/components/pokemonDetailComponents/FrontSprite.js
+++ src/components/pokemonDetailComponents/FrontSprite.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { StyleSheet, Image } from 'react-native';
 
 const FrontSprite = ({ results, width, height }) => {
+  const sprite = (el) => {
+    if (el.sprites.versions["generation-v"]["black-white"].animated.front_default === null) {
+      return el.sprites.front_default
+    } else {
+      return el.sprites.versions["generation-v"]["black-white"].animated.front_default
+    }
+  }
 
   return (
-    <Image style={[styles.imageStyle, { width: width, height: height }]} source={{ uri: results.sprites.front_default }} />
+    <Image 
+      style={[styles.imageStyle, { width: width, height: height, overflow: 'hidden' }]} 
+      resizeMode='contain' 
+      source={{ uri: sprite(results) }} 
+    />
   );
 };
 
