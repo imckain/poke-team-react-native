67d7dba2a2811481d2fdf4228112316db11248c1
imckain <ianmmckain@gmail.com>
2021-10-02 23:50:19 -0400

    created search params, passing in new searchBars

--- src/screens/AdvancedSearchScreen.js
+++ src/screens/AdvancedSearchScreen.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Pressable, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import SearchBar from '../components/navigatorCards/SearchBar';
+import { Text, View, StyleSheet, Pressable, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+import SearchBarByName from '../components/navigatorCards/searchBars/SearchBarByName';
+import SearchBarByType from '../components/navigatorCards/searchBars/SearchBarByType';
+import SearchBarByMove from '../components/navigatorCards/searchBars/SearchBarByMove';
+import SearchBarByAbility from '../components/navigatorCards/searchBars/SearchBarByAbility';
+import SearchBarByBerry from '../components/navigatorCards/searchBars/SearchBarByBerry';
 import ShowAdvancedSearchResult from '../components/teamBuilder/ShowAdvancedSearchResult';
 
-import useResults from '../hooks/useResults';
+import useAdvancedResults from '../hooks/useAdvancedResults';
+import pokemonData from '../data/pokemon.json';
+import PokedexCard from '../pokedex/PokedexCard';
 
 const HideKeyboard = ({ children }) => (
   <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -13,19 +21,123 @@ const HideKeyboard = ({ children }) => (
 
 const AdvancedSearchScreen = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [advancedSearchAPI, advancedResults] = useResults([]);
+  const [advancedSearchAPI, advancedResults] = useAdvancedResults([]);
+  const [searchParam, setSearchParam] = useState('pokemon');
+
+  const searchSelection = (param) => {
+    if (param === 'pokemon') {
+      return <SearchBarByName 
+        searchTerm={searchTerm} 
+        onSearchTermChange={setSearchTerm} 
+        onSearchTermSubmit={() => advancedSearchAPI(searchParam, searchTerm)}
+        style={styles.searchBar}
+      />
+    }
+    if (param === 'type') {
+      return <SearchBarByType 
+        searchTerm={searchTerm} 
+        onSearchTermChange={setSearchTerm} 
+        onSearchTermSubmit={() => advancedSearchAPI(searchParam, searchTerm)}
+        style={styles.searchBar}
+      />
+    }
+    if (param === 'move') {
+      return <SearchBarByMove 
+        searchTerm={searchTerm} 
+        onSearchTermChange={setSearchTerm} 
+        onSearchTermSubmit={() => advancedSearchAPI(searchParam, searchTerm)}
+        style={styles.searchBar}
+      />
+    }
+    if (param === 'ability') {
+      return <SearchBarByAbility 
+        searchTerm={searchTerm} 
+        onSearchTermChange={setSearchTerm} 
+        onSearchTermSubmit={() => advancedSearchAPI(searchParam, searchTerm)}
+        style={styles.searchBar}
+      />
+    }
+    if (param === 'berry') {
+      return <SearchBarByBerry 
+        searchTerm={searchTerm} 
+        onSearchTermChange={setSearchTerm} 
+        onSearchTermSubmit={() => advancedSearchAPI(searchParam, searchTerm)}
+        style={styles.searchBar}
+      />
+    }
+  }
+
+  const showPokeDex = (param) => {
+    if (param === 'pokemon') {
+      return <FlatList 
+        horizontal={false}
+        data={pokemonData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return(
+            <Pressable>
+              <PokedexCard results={item} />
+            </Pressable>
+          )
+        }}
+      />
+    }
+  }
+
+  const showPokemonCard = (param) => {
+    if (param !== '') {
+      return <FlatList 
+        horizontal={false}
+        data={advancedResults}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return(
+            <Pressable onPress={() => props.navigation.navigate('Detail Modal', { results: item })}>
+              <ShowAdvancedSearchResult results={item} />
+            </Pressable>
+          )
+        }}
+      />
+    } else {
+      return null;
+    }
+  }
 
   return (
     <HideKeyboard>
       <View style={styles.container}>
-        <SearchBar 
-          searchTerm={searchTerm} 
-          onSearchTermChange={setSearchTerm} 
-          onSearchTermSubmit={() => advancedSearchAPI(searchTerm)}
-        />
-        <Pressable onPress={() => props.navigation.navigate('Detail Modal', { results: advancedResults })}>
-          <ShowAdvancedSearchResult results={advancedResults} />
-        </Pressable>
+        <View style={styles.searchBarContainer}>
+          {searchSelection(searchParam)}
+          <View style={styles.searchParamsContainter}>
+            <Pressable onPress={() => setSearchParam('pokemon')}>
+              <View style={[searchParam === 'pokemon' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>
+                <Text style={[searchParam === 'pokemon' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>Pokemon</Text>
+              </View>
+            </Pressable>
+            <Pressable onPress={() => setSearchParam('type')}>
+              <View style={[searchParam === 'type' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>
+                <Text style={[searchParam === 'type' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>Type</Text>
+              </View>
+            </Pressable>
+            <Pressable onPress={() => setSearchParam('move')}>
+              <View style={[searchParam === 'move' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>
+                <Text style={[searchParam === 'move' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>Move</Text>
+              </View>
+            </Pressable>
+            <Pressable onPress={() => setSearchParam('ability')}>
+              <View style={[searchParam === 'ability' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>
+                <Text style={[searchParam === 'ability' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>Ability</Text>
+              </View>
+            </Pressable>
+            <Pressable onPress={() => setSearchParam('berry')}>
+              <View style={[searchParam === 'berry' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>
+                <Text style={[searchParam === 'berry' ? styles.activeSearchParamText : styles.inactiveSearchParamText]}>Berry</Text>
+              </View>
+            </Pressable>
+          </View>
+        </View>
+        {showPokemonCard(searchTerm)}
+        {showPokeDex(searchParam)}
       </View>
     </HideKeyboard>
   );
@@ -34,8 +146,39 @@ const AdvancedSearchScreen = (props) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#353340',
+    flexDirection: 'column',
     flex: 1
   },
+  searchBarContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: 100,
+    marginBottom: 18
+  },
+  searchBar:{
+    paddingHorizontal: 6
+  },
+  searchSettingIcon: {
+    paddingRight: 20
+  },
+  searchParamsContainter: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-evenly'
+  },
+  activeSearchParamText: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: '600',
+    borderBottomWidth: 2,
+    borderColor: '#fff',
+    paddingBottom: 1,
+  },
+  inactiveSearchParamText: {
+    color: 'rgb(175, 175, 175)',
+    fontSize: 22,
+    fontWeight: '400',
+  }
 });
 
 export default React.memo(AdvancedSearchScreen);
