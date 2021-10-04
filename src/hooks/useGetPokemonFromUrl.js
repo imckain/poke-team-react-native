c21cf945b4591f7da8bd301254f7100245023f40
imckain <ianmmckain@gmail.com>
2021-10-04 16:17:09 -0400

    hook for calling api on specific pokemon from other screens

---
+++ src/hooks/useGetPokemonFromUrl.js
@@ -0,0 +1,24 @@
+import { useCallback, useEffect, useState } from 'react';
+import pokeApi from '../api/pokeApi';;
+
+export default () => {
+  const [pokemonResults, setPokemonResults] = useState([]);
+  const [advancedApiErrorMessage, setAdvancedApiErrorMessage] = useState('');
+  
+  const getPokemonFromUrl = useCallback(async (defaultTerm) => {
+    if (defaultTerm === '') { return null }
+    try {
+      const response = await pokeApi.get(`${defaultTerm}`);
+      console.log('Hook: ' + response);
+      setPokemonResults([response.data])
+    } catch (error) {
+      setAdvancedApiErrorMessage('Something Went Wrong')
+    }
+  }, []);
+
+  useEffect(() => {
+    getPokemonFromUrl('');
+  }, []);
+  
+  return [getPokemonFromUrl, pokemonResults]
+};
