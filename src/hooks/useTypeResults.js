414c7dc4d2d4af877a2101e7ecd6695e3bbf5ec5
imckain <ianmmckain@gmail.com>
2021-10-04 16:17:31 -0400

    hook for handling type searching

---
+++ src/hooks/useTypeResults.js
@@ -0,0 +1,24 @@
+import { useCallback, useEffect, useState } from 'react';
+import pokeApi from '../api/pokeApi';;
+
+export default () => {
+  const [typeResults, setTypeResults] = useState([]);
+  const [advancedApiErrorMessage, setAdvancedApiErrorMessage] = useState('');
+  
+  const typeSearchApi = useCallback(async (defaultTerm) => {
+    if (defaultTerm === '') { return null }
+    try {
+      const response = await pokeApi.get(`https://pokeapi.co/api/v2/type/${defaultTerm}`);
+      console.log(response.data.name);
+      setTypeResults([response.data])
+    } catch (error) {
+      setAdvancedApiErrorMessage('Something Went Wrong')
+    }
+  }, []);
+
+  useEffect(() => {
+    typeSearchApi('');
+  }, []);
+  
+  return [typeSearchApi, typeResults]
+};
