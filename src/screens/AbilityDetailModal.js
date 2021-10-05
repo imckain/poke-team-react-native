8a6f20132b09684e4412a174b1396ea3d04a324c
imckain <ianmmckain@gmail.com>
2021-10-05 13:37:40 -0400

    ability detail modal

---
+++ src/screens/AbilityDetailModal.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import AbilityEffect from '../components/abilityDetailComponents/AbilityEffect';
+
+import AbilityName from '../components/abilityDetailComponents/AbilityName';
+import AbilityPokemon from '../components/abilityDetailComponents/AbilityPokemon';
+
+const MoveDetailModal = (props) => {
+  let results;
+
+  if (props.route.params.results[0] === undefined) {
+    results = props.route.params.results
+  } else results = props.route.params.results[0]
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollViewContainer}>
+        <View style={styles.mainInfo}>
+          <AbilityName results={results} />
+          <AbilityEffect results={results} />
+          <AbilityPokemon results={results} />
+        </View>
+        <View style={styles.detailInfo}>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+    backgroundColor: '#353340',
+    paddingHorizontal: 10,
+  },
+  spriteContainer: {
+    flexDirection: 'row'
+  },
+  mainInfo: {
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    maxWidth: '100%',
+    paddingTop: 20
+  },
+  detailInfo: {
+    flex: 1,
+    paddingLeft: 5,
+    paddingVertical: 8,
+    paddingBottom: 300
+  },
+});
+
+export default React.memo(MoveDetailModal);
