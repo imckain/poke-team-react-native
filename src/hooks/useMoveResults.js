1cdb4e71e36307ea9a5c0d2f3a9fc422d4cdf62f
imckain <ianmmckain@gmail.com>
2021-10-05 13:37:00 -0400

    hook for handling move search api request

---
+++ src/hooks/useMoveResults.js
@@ -0,0 +1,24 @@
+import { useCallback, useEffect, useState } from 'react';
+import pokeApi from '../api/pokeApi';;
+
+export default () => {
+  const [moveResults, setMoveResults] = useState([]);
+  const [advancedApiErrorMessage, setAdvancedApiErrorMessage] = useState('');
+  
+  const moveSearchApi = useCallback(async (defaultTerm) => {
+    if (defaultTerm === '') { return null }
+    try {
+      const response = await pokeApi.get(`https://pokeapi.co/api/v2/move/${defaultTerm}`);
+      console.log(response.data.name);
+      setMoveResults([response.data])
+    } catch (error) {
+      setAdvancedApiErrorMessage('Something Went Wrong')
+    }
+  }, []);
+
+  useEffect(() => {
+    moveSearchApi('');
+  }, []);
+  
+  return [moveSearchApi, moveResults]
+};
