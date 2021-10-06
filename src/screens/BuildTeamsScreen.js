a438bfca4b0f1d3a65bd6075c5852e58a2c7d6e5
imckain <ianmmckain@gmail.com>
2021-10-06 16:54:20 -0400

    added components for UI on buildteams screen

--- src/screens/BuildTeamsScreen.js
+++ src/screens/BuildTeamsScreen.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity, TouchableWithoutFeedback, Pressable, Keyboard } from 'react-native';
+import { Text, View, StyleSheet, Image, FlatList, ScrollView, TouchableWithoutFeedback, Pressable, Keyboard } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import BuildTeamSearchBar from '../components/buildTeamComponents/BuildTeamSearchBar';
 import useBuildResults from '../hooks/useBuildResults';
 import ShowAdvancedSearchResult from '../components/resultsCards/ShowAdvancedSearchResult';
+import BuildTeamsButton from '../components/buildTeamComponents/BuildTeamsButton';
+import AddPokemonButton from '../components/buildTeamComponents/AddPokemon';
+import PokemonSlotCard from '../components/buildTeamComponents/PokemonSlotCard';
+import { TextInput } from 'react-native-gesture-handler';
 
 const HideKeyboard = ({ children }) => (
   <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -12,6 +18,7 @@ const HideKeyboard = ({ children }) => (
 
 const BuildTeamsScreen = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [teamName, setTeamName] = useState('');
   const [buildSearchApi, buildResults] = useBuildResults([]);
 
   const showPokemonCard = (param) => {
@@ -20,12 +27,18 @@ const BuildTeamsScreen = (props) => {
         horizontal={false}
         scrollEnabled={false}
         data={buildResults}
+        style={{height: 'auto'}}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => {
           return(
-            <Pressable onPress={() => props.navigation.navigate('Detail Modal', { results: item })}>
-              <ShowAdvancedSearchResult results={item} />
-            </Pressable>
+            <View style={styles.addMonCard}>
+              <Pressable style={{flex: 1}} onPress={() => props.navigation.navigate('Detail Modal', { results: item })}>
+                <ShowAdvancedSearchResult results={item} />
+              </Pressable>
+              <Pressable>
+                <AddPokemonButton name={item.name} width={'90%'} height={40} />
+              </Pressable>
+            </View>
           )
         }}
       />
@@ -34,9 +47,34 @@ const BuildTeamsScreen = (props) => {
     }
   }
 
+  const storePokemon = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@pokemon', jsonValue)
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <HideKeyboard>
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
+        <View style={styles.teamNameContainer}>
+          <TextInput 
+            placeholder={'Team 1'} 
+            showSoftInputOnFocus={false}
+            autoCapitalize='none'
+            autoCorrect={false}
+            style={styles.inputStyle} 
+            placeholderTextColor='rgb(175, 175, 175)'
+            value={teamName}
+            onChangeText={setTeamName}
+            // onEndEditing={onSearchTermSubmit}
+            // clearButtonMode='always'
+            keyboardAppearance='dark'
+            returnKeyType={'done'}
+          />
+        </View>
         <View style={styles.searchBarContainer}>
           <BuildTeamSearchBar 
           searchTerm={searchTerm} 
@@ -45,9 +83,19 @@ const BuildTeamsScreen = (props) => {
           style={styles.searchBar}
           />
         </View>
-        {showPokemonCard(searchTerm)}
+        <View style={{height: 'auto'}}>
+          {showPokemonCard(searchTerm)}
+        </View>
         <View style={{height: 5 }} />
-      </View>
+        <View style={styles.teamSlotContainer}>
+          <PokemonSlotCard results={buildResults} />
+          <PokemonSlotCard results={buildResults} />
+          <PokemonSlotCard results={buildResults} />
+          <PokemonSlotCard results={buildResults} />
+          <PokemonSlotCard results={buildResults} />
+          <PokemonSlotCard results={buildResults} />
+        </View>
+      </ScrollView>
     </HideKeyboard>
   );
 };
@@ -56,7 +104,17 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#353340',
     flexDirection: 'column',
-    flex: 1
+    flex: 1,
+    paddingTop: 20,
+    width: '100%',
+  },
+  teamSlotContainer: {
+    marginBottom: 90
+  },
+  addMonCard: {
+    flexDirection: 'column',
+    width: '100%',
+    justifyContent: 'center',
   },
   searchBarContainer: {
     flexDirection: 'column',
@@ -67,6 +125,23 @@ const styles = StyleSheet.create({
   searchBar:{
     paddingHorizontal: 6
   },
+  teamNameContainer: {
+    height: 'auto',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginVertical: 20,
+  },
+  inputStyle: {
+    height: '100%',
+    flex: 1,
+    fontSize: 60,
+    textAlign: 'center',
+    fontWeight: '500',
+    color: '#fff',
+  }
 });
 
 export default React.memo(BuildTeamsScreen);
