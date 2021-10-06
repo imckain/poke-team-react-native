5a39dd07556daea6d75a4b1eaffcdedaeca909e6
imckain <ianmmckain@gmail.com>
2021-10-05 21:02:10 -0400

    changed folder name for readability

---
+++ src/components/resultsCards/ShowTypeSearchResult.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+
+import BaseStats from '../pokemonDetailComponents/BaseStats';
+import TypeDetail from '../pokemonDetailComponents/TypeDetail';
+import PokemonNameAndId from '../pokemonDetailComponents/PokemonNameAndId';
+import FrontSprite from '../pokemonDetailComponents/FrontSprite';
+import TypeNameAndClass from '../typeDetailComponents/TypeNameAndClass';
+import TypeMoves from '../typeDetailComponents/TypeMoves';
+
+const ShowAdvancedTypeResult = (props) => {
+  const results = props.results
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <View style={styles.mainInfo}>
+          <TypeNameAndClass results={results} />
+          {/* <FrontSprite width={150} height={150} results={results} /> */}
+        </View>
+        <View style={styles.detailInfo}>
+          {/* <BaseStats headerFontSize={22} detailFontSize={16} results={results} />
+          <TypeDetail margin={7} headerFontSize={22} detailFontSize={16} results={results} /> */}
+        </View>
+        <Text style={styles.infoNotice}>Tap for more info</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    borderRadius: 10,
+    width: '90%',
+    height: 100,
+    marginBottom: 90,
+    alignSelf: 'center',
+    backgroundColor: '#464450',
+  },
+  mainInfo: {
+    justifyContent: 'flex-start',
+    maxWidth: '100%'
+  },
+  detailInfo: {
+    // flex: 1,
+    paddingHorizontal: 5,
+    paddingVertical: 8,
+  },
+  infoNotice: {
+    textAlign: 'center',
+    color: '#ffffffa7',
+    fontStyle: 'italic',
+    paddingTop: 2,
+  }
+});
+
+export default React.memo(ShowAdvancedTypeResult);
