2e1b891e539c1e02452c76c32ef62638620c375d
imckain <ianmmckain@gmail.com>
2021-10-06 16:54:04 -0400

    added components for UI on teams screen

--- src/screens/TeamsScreen.js
+++ src/screens/TeamsScreen.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Text, View, StyleSheet, Pressable } from 'react-native';
-
-import BuildTeamsNavigator from '../components/navigatorCards/BuildTeamsNavigator';
+import { ScrollView } from 'react-native-gesture-handler';
+import BuildTeamsButton from '../components/buildTeamComponents/BuildTeamsButton';
+import ViewTeams from '../components/buildTeamComponents/ViewTeams';
 
 const TeamsScreen = (props) => {
 
   return (
     <View style={styles.container}>
-      <Text>Teams</Text>
-      <Pressable onPress={() => props.navigation.navigate('Home')} >
-        <Text>Home</Text>
-      </Pressable>
+      <ScrollView contentContainerStyle={{justifyContent: 'center', alignItems: 'center'}} style={styles.teamsScrollView}>
+        <View style={styles.teamsView}>
+          <ViewTeams height={'auto'} width={'100%'} />
+          <ViewTeams height={'auto'} width={'100%'} />
+          <ViewTeams height={'auto'} width={'100%'} />
+          <ViewTeams height={'auto'} width={'100%'} />
+          <ViewTeams height={'auto'} width={'100%'} />
+        </View>
+      </ScrollView>
       <Pressable style={styles.buttonStyle} onPress={() => props.navigation.navigate('Build a Team')}>
-        <BuildTeamsNavigator />
+        <BuildTeamsButton height={80} width={'100%'} />
       </Pressable>
     </View>
   );
@@ -20,14 +26,31 @@ const TeamsScreen = (props) => {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
     backgroundColor: '#353340',
-    flex: 1
+    flex: 1,
+    justifyContent: 'space-between'
   },
   buttonStyle: {
     fontSize: 32,
     fontWeight: '600',
     textAlign: 'center',
-    alignSelf: 'center'
+    alignSelf: 'center',
+    width: '100%',
+    paddingHorizontal: 12,
+    paddingTop: 16
+  },
+  teamsScrollView: {
+    width: '100%',
+    alignSelf: 'center',
+  },
+  teamsView: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '90%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginTop: 24,
   },
 });
 
