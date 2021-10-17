55895526ecaa1b054738e1b4bafbac85b12ea741
imckain <ianmmckain@gmail.com>
2021-10-17 15:11:20 -0400

    bug fix- checking for null attribites

--- src/components/moveDetailComponents/MoveAttributes.js
+++ src/components/moveDetailComponents/MoveAttributes.js
@@ -3,9 +3,14 @@ import { Text, StyleSheet, View } from 'react-native';
 
 const MoveAttributes = ({ results, fontSize }) => {
 
+  const checkForNull = (el) => {
+    if (el === null) return 'N/A'
+    else return el
+  }
+
   return (
     <View style={styles.container}>
-      <Text allowFontScaling={false} style={[styles.attributes, { fontSize: fontSize }]}> Power: {results.power} | PP: {results.pp} | Acc: {results.accuracy}</Text>
+      <Text allowFontScaling={false} style={[styles.attributes, { fontSize: fontSize }]}> Power: {checkForNull(results.power)} | PP: {checkForNull(results.pp)} | Acc: {checkForNull(results.accuracy)}</Text>
     </View>
   );
 };
