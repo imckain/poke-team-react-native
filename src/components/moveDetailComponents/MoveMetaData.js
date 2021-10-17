37e0bbc66dad2d86193ee775ec36c08ea9eb341f
imckain <ianmmckain@gmail.com>
2021-10-17 15:11:35 -0400

    bug fix- checking for null attribites

--- src/components/moveDetailComponents/MoveMetaData.js
+++ src/components/moveDetailComponents/MoveMetaData.js
@@ -24,8 +24,34 @@ const MoveMetaData = ({ results }) => {
   }, []);
 
   const checkForNull = (el) => {
-    if (el === null) return 'N/A'
-    else return el
+    if (el.meta === null) {
+      return <Text allowFontScaling={false} style={styles.subHeaderText}>Meta Data Not Available</Text>
+    }
+    if (el === null) {
+      return <Text allowFontScaling={false} style={styles.subHeaderText}>N/A</Text>
+    }
+    else return (
+      <View>
+        <Text allowFontScaling={false} style={styles.subHeaderText}>Ailment: {el.meta.ailment.name}</Text>
+        {checkForCollapse(collapsed)}
+        <Collapsible collapsed={collapsed}>
+          <View style={styles.scrollViewStyle}>
+            <Text allowFontScaling={false} style={styles.descriptionText}>{el.effect_entries[0].effect}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Category: {el.meta.category.name}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Inflict Chance: {el.meta.ailment_chance}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Crit Rate: {el.meta.crit_rate}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Drain: {el.meta.drain}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Flinch Chance: {el.meta.flinch_chance}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Stat Chance: {el.meta.stat_chance}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Healing: {el.meta.healing}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Max Hit: {el.meta.max_hits}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Max Turns: {el.meta.max_turns}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Min Hit: {el.meta.min_hits}</Text>
+            <Text allowFontScaling={false} style={styles.metaDataText}>Min Turns: {el.meta.min_turns}</Text>
+          </View>
+        </Collapsible>
+      </View>
+    )
   }
   
   function Capitalize(str) {
@@ -34,24 +60,25 @@ const MoveMetaData = ({ results }) => {
 
   return (
     <View style={[styles.container]}>
-      <Text allowFontScaling={false} style={styles.subHeaderText}>Ailment: {results.meta.ailment.name}</Text>
+      {checkForNull(results)}
+      {/* <Text allowFontScaling={false} style={styles.subHeaderText}>Ailment: {checkForNull(results, results.meta.ailment.name)}</Text>
       {checkForCollapse(collapsed)}
       <Collapsible collapsed={collapsed}>
         <View style={styles.scrollViewStyle}>
-          <Text allowFontScaling={false} style={styles.descriptionText}>{results.effect_entries[0].effect}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Category: {results.meta.category.name}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Inflict Chance: {results.meta.ailment_chance}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Crit Rate: {results.meta.crit_rate}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Drain: {results.meta.drain}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Flinch Chance: {results.meta.flinch_chance}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Stat Chance: {results.meta.stat_chance}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Healing: {results.meta.healing}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Max Hit: {checkForNull(results.meta.max_hits)}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Max Turns: {checkForNull(results.meta.max_turns)}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Min Hit: {checkForNull(results.meta.min_hits)}</Text>
-          <Text allowFontScaling={false} style={styles.metaDataText}>Min Turns: {checkForNull(results.meta.min_turns)}</Text>
+          <Text allowFontScaling={false} style={styles.descriptionText}>{checkForNull(results, results.effect_entries[0].effect)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Category: {checkForNull(results, results.meta.category.name)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Inflict Chance: {checkForNull(results, results.meta.ailment_chance)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Crit Rate: {checkForNull(results, results.meta.crit_rate)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Drain: {checkForNull(results, results.meta.drain)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Flinch Chance: {checkForNull(results, results.meta.flinch_chance)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Stat Chance: {checkForNull(results, results.meta.stat_chance)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Healing: {checkForNull(results, results.meta.healing)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Max Hit: {checkForNull(results, results.meta.max_hits)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Max Turns: {checkForNull(results, results.meta.max_turns)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Min Hit: {checkForNull(results, results.meta.min_hits)}</Text>
+          <Text allowFontScaling={false} style={styles.metaDataText}>Min Turns: {checkForNull(results, results.meta.min_turns)}</Text>
         </View>
-      </Collapsible>
+      </Collapsible> */}
     </View>
   );
 };
@@ -62,7 +89,7 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     width: '100%',
     marginHorizontal: 12,
-    marginBottom: 10
+    marginBottom: 16
   },
   subHeaderText: {
     color: '#fff',
@@ -74,7 +101,7 @@ const styles = StyleSheet.create({
     marginTop: 6,
     marginLeft: 22,
     fontWeight: '600',
-    fontSize: 16,
+    fontSize: 18,
   },
   descriptionText: {
     color: '#fff',
