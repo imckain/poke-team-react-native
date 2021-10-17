37a5bc4844ebc10b74beace9b36994985510dc0c
imckain <ianmmckain@gmail.com>
2021-10-17 15:44:34 -0400

    bug fix- checking for null

--- src/components/abilityDetailComponents/AbilityEffect.js
+++ src/components/abilityDetailComponents/AbilityEffect.js
@@ -4,14 +4,17 @@ import { Text, StyleSheet, View } from 'react-native';
 const AbilityEffect = ({ results }) => {
 
   const checkLanguage = (el) => {
+    if (el.effect_entries[0] === null) return 'N/A'
     if (el.effect_entries[0] !== undefined) {
       const filter = el.effect_entries.filter((item) => item.language.name === 'en')
       return filter[0].effect
     } 
     if (el.effect_entries[0] === undefined) {
-      const filter = el.flavor_text_entries.filter((item) => item.language.name === 'en')
-      return filter[0].flavor_text
-    } 
+      if (el.flavor_text_entries[0] === null) {
+        const filter = el.flavor_text_entries.filter((item) => item.language.name === 'en')
+        return filter[0].flavor_text
+      } else return 'Description not available'
+    }
   }
 
   return (
