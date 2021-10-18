603b2878d793b060ca4f33419a538d1370fd9466
imckain <ianmmckain@gmail.com>
2021-10-17 21:38:30 -0400

    created new component to show on results cards that show a non clickable, more plain view of types for a pokemon

---
+++ src/components/pokemonDetailComponents/TypeShowOnCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const TypeShowOnCard = ({ results, margin, headerFontSize, detailFontSize }) => {
+  function Capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  return (
+    <View style={[styles.typeContainer, { marginBottom: margin }]}>
+      <Text allowFontScaling={false} style={[styles.typeLabelText, { fontSize: headerFontSize }]}>Type:</Text>
+      <Text allowFontScaling={false} style={[styles.typeText, { fontSize: detailFontSize }]}>{results.types.map(el => Capitalize(el.type.name) + ' ')}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  typeContainer: {
+    flexDirection: 'row',
+    alignItems: 'baseline'
+  },
+  typeLabelText: {
+    color: '#fff',
+    marginTop: 7,
+    fontWeight: '600'
+  },
+  typeText: {
+    color: '#fff',
+    marginLeft: 7,
+  },
+});
+
+export default TypeShowOnCard;
