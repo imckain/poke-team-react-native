90ea8358d7dc7c55171e208b210bc3178b09841e
imckain <ianmmckain@gmail.com>
2021-10-17 21:40:17 -0400

    added clickable type cards that link to type detail

--- src/components/moveDetailComponents/MoveType.js
+++ src/components/moveDetailComponents/MoveType.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { Text, StyleSheet, View, Pressable } from 'react-native';
 
-const MoveType = ({ results }) => {
+import useGetReultsFromUrl from '../../hooks/useGetResultsFromUrl';
+
+const MoveType = ({ results, navigation, detailFontSize, headerFontSize, margin, flexDirection }) => {
+  const [getResultsFromUrl, urlResults] = useGetReultsFromUrl();
+
   function Capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
@@ -12,28 +16,70 @@ const MoveType = ({ results }) => {
     } else return Capitalize(el.type.name)
   }
 
+  const showType = (el) => {
+    const searchApiByUrl = useCallback(async(term) => {
+      await getResultsFromUrl(term);
+      return urlResults
+    }, [])
+
+    const navigate = async(url) => {
+      if (urlResults.id !== undefined) {
+        return navigation.navigate('Type Detail Modal', { results: urlResults })
+      } else searchApiByUrl(url)
+    }
+
+    return (
+      <View key={el.type.name} style={styles.typeBox}>
+        <Pressable 
+          onPressIn={async() => {
+            await searchApiByUrl(el.type.url)
+          }}
+          onPressOut={() => navigate(el.type.url)}
+        >
+          <Text allowFontScaling={false} style={[styles.typeText, { fontSize: detailFontSize }]}>{isValid(el) + ' '}</Text>
+        </Pressable>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.typeView}>
-      <Text allowFontScaling={false} style={[styles.type]}>Type:  <Text style={styles.typeSub}>{isValid(results)}</Text></Text>
+    <View style={[styles.typeContainer, { marginBottom: margin, flexDirection: flexDirection }]}>
+    <Text allowFontScaling={false} style={[styles.typeLabelText, { fontSize: headerFontSize }]}>Type:</Text>
+    <View style={{flexDirection: 'row'}}>
+      {showType(results)}
     </View>
+  </View>
   );
 };
 
 const styles = StyleSheet.create({
-  typeView: {
-    marginBottom: 10,
+  typeContainer: {
+    flexDirection: 'column',
+    alignItems: 'baseline',
+    marginHorizontal: 12,
   },
-  type: {
+  typeLabelText: {
     color: '#fff',
-    fontSize: 22,
-    fontWeight: '600',
-    marginHorizontal: 12
+    marginTop: 7,
+    fontWeight: '600'
   },
-  typeSub: {
+  typeBox: {
+    paddingVertical: 5,
+    borderRadius: 10,
+    backgroundColor: '#464450',
+    margin: 7,
+    alignContent: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    width: 'auto'
+  },
+  typeText: {
     color: 'rgb(223, 223, 223)',
     fontSize: 18,
-    fontWeight: '400',
-    marginHorizontal: 12
+    fontWeight: '600',
+    textAlign: 'center',
+    paddingHorizontal: 12,
+    paddingVertical: 3
   },
 });
 
