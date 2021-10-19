7f0bc70866b8e2c516efcf699b3cac1fd54258da
imckain <ianmmckain@gmail.com>
2021-10-19 00:54:02 -0400

    created uuid for each new team

--- src/context/TeamContext.js
+++ src/context/TeamContext.js
@@ -1,9 +1,13 @@
+import uuid from 'react-native-uuid'
+
 import createDataContext from './createDataContext';
 
 const teamsReducer = (state, action) => {
   switch (action.type) {
     case 'add_team':
-      return [...state, { name: `Team #${state.length + 1}`}];
+      return [...state, 
+        { 
+          id: uuid.v4(), name: `Team #${state.length + 1}`}];
     default:
       return state;
   }
