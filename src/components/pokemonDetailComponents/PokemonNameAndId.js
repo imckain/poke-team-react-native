6a235f3be1ace4501cb4716e31204aba96688bae
imckain <ianmmckain@gmail.com>
2021-10-19 23:04:45 -0400

    refactored UI, imporoved UX

--- src/components/pokemonDetailComponents/PokemonNameAndId.js
+++ src/components/pokemonDetailComponents/PokemonNameAndId.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const PokemonNameAndId = ({ results, fontSize }) => {
   function Capitalize(str) {
@@ -7,7 +7,10 @@ const PokemonNameAndId = ({ results, fontSize }) => {
   }
 
   return (
-    <Text adjustsFontSizeToFit={true} allowFontScaling={false} style={[styles.name, {fontSize: fontSize}]}>{Capitalize(results.name.replace('-', ' '))} #{results.id}</Text>
+    <View style={{width: '100%' ,flexDirection: 'column', justifyContent: 'center'}}>
+      <Text adjustsFontSizeToFit={true} allowFontScaling={false} style={[styles.name, {fontSize: fontSize}]}>{Capitalize(results.name.replace('-', ' '))}</Text>
+      <Text style={[styles.name, {color: 'rgba(255, 255, 255, 0.5)'}]}>{results.id}</Text>
+    </View>
   );
 };
 
@@ -15,10 +18,9 @@ const styles = StyleSheet.create({
   name: {
     textAlign: 'center',
     color: '#fff',
-    fontSize: 22,
+    fontSize: 18,
     fontWeight: '600',
-    marginVertical: 8,
-    marginHorizontal: 12
+    // marginBottom: 12
   },
 });
 
