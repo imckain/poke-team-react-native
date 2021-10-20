2fbd88d1ac6e0df04ee1fbdbd0b20908b244b269
imckain <ianmmckain@gmail.com>
2021-10-20 19:49:28 -0400

    UI- changed card design and alignment

--- src/pokedex/PokedexAbility.js
+++ src/pokedex/PokedexAbility.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
 
 const PokedexAbility = ({ results, fontSize }) => {
   function Capitalize(str) {
@@ -7,18 +9,28 @@ const PokedexAbility = ({ results, fontSize }) => {
   }
 
   return (
-    <Text allowFontScaling={false} style={[styles.name, {fontSize: fontSize}]}>{Capitalize(results.identifier.replace('-', ' '))}</Text>
+    <View style={styles.container}>
+      <View style={{flexDirection: 'row', alignItems: 'baseline'}}>
+        <Text allowFontScaling={false} style={[styles.name, {fontSize: fontSize}]}>{Capitalize(results.identifier.replace('-', ' '))}</Text>
+      </View>
+      <Ionicons style={{ paddingHorizontal: 12 }} name="ios-chevron-forward-sharp" size={18} color="rgb(175, 175, 175)" />  
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    width: '100%'
+  },
   name: {
+    marginHorizontal: 12,
+    marginVertical: 6,
     textAlign: 'center',
     color: '#fff',
-    fontSize: 32,
     fontWeight: '600',
-    marginVertical: 8,
-    marginHorizontal: 12
   },
 });
 
