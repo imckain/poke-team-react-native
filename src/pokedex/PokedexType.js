599cd512e19c3d8d23b86c844106d842135b73ea
imckain <ianmmckain@gmail.com>
2021-10-20 19:46:25 -0400

    UI- color of name is set by its type

--- src/pokedex/PokedexType.js
+++ src/pokedex/PokedexType.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import checkType from '../components/functions/checkType';
+
+import { Ionicons } from '@expo/vector-icons';
 
 const PokedexType = ({ results, fontSize }) => {
   function Capitalize(str) {
@@ -7,18 +10,31 @@ const PokedexType = ({ results, fontSize }) => {
   }
 
   return (
-    <Text allowFontScaling={false} style={[styles.name, {fontSize: fontSize}]}>{Capitalize(results.identifier)}</Text>
+    <View style={styles.container}>
+      <View style={{flexDirection: 'row', alignItems: 'baseline'}}>
+        <Text allowFontScaling={false} style={[styles.name, {fontSize: fontSize, color: checkType(results.identifier) }]}>{Capitalize(results.identifier)}</Text>
+      </View>
+      <Ionicons style={{ paddingHorizontal: 12 }} name="ios-chevron-forward-sharp" size={18} color="rgb(175, 175, 175)" />  
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    width: '100%'
+  },
   name: {
     textAlign: 'center',
     color: '#fff',
-    fontSize: 32,
     fontWeight: '600',
-    marginVertical: 8,
-    marginHorizontal: 12
+    marginHorizontal: 12,
+    marginVertical: 6,
+    textShadowColor: 'rgb(34, 34, 34)',
+    textShadowOffset: { width: 0, height: 0},
+    textShadowRadius: 3,
   },
 });
 
