7f3f04464aebb4a40f3e83ae2a89d0988a1c3de6
imckain <ianmmckain@gmail.com>
2021-10-20 19:53:05 -0400

    UI design change- header font changes color based on type

--- src/components/typeDetailComponents/TypeNameAndClass.js
+++ src/components/typeDetailComponents/TypeNameAndClass.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import checkType from '../functions/checkType';
 
 const TypeName = ({ results, alignSelf, fontSize }) => {
   function Capitalize(str) {
@@ -14,34 +15,43 @@ const TypeName = ({ results, alignSelf, fontSize }) => {
 
   return (
     <View style={[styles.dmgClassView, { alignSelf: alignSelf }]}>
-      <Text allowFontScaling={false} style={[styles.name, { fontSize: fontSize, alignSelf: alignSelf }]}>{Capitalize(results.name)}</Text>
+      <Text allowFontScaling={false} style={[styles.name, { fontSize: fontSize, alignSelf: alignSelf, color: checkType(results.name) }]}>{Capitalize(results.name)}</Text>
       <Text allowFontScaling={false} style={[styles.damageClass]}>Damage Class:  <Text style={styles.damageClassSub}>{isValid(results)}</Text></Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  name: {
-    color: '#fff',
-    fontWeight: '600',
-    marginVertical: 8,
-    alignSelf: 'center'
-  },
   dmgClassView: {
     marginBottom: 10,
     alignSelf: 'center',
     marginBottom: 12,
   },
+  name: {
+    color: '#fff',
+    fontWeight: '600',
+    marginVertical: 8,
+    alignSelf: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 0.712)',
+    textShadowOffset: { width: 0, height: 0},
+    textShadowRadius: 3,
+  },
   damageClass: {
     color: '#fff',
     fontSize: 16,
     fontWeight: '600',
+    textShadowColor: 'rgba(0, 0, 0, 0.712)',
+    textShadowOffset: { width: 0, height: 0},
+    textShadowRadius: 3,
   },
   damageClassSub: {
     color: 'rgb(223, 223, 223)',
     fontSize: 16,
     fontWeight: '400',
-    marginHorizontal: 12
+    marginHorizontal: 12,
+    textShadowColor: 'rgba(0, 0, 0, 0.712)',
+    textShadowOffset: { width: 0, height: 0},
+    textShadowRadius: 3,
   },
 });
 
