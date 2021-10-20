f0454dfccf55db6d5890eb2a423a9556eac14c09
imckain <ianmmckain@gmail.com>
2021-10-20 19:46:36 -0400

    UI- color of name is set by its type

--- src/pokedex/PokedexMove.js
+++ src/pokedex/PokedexMove.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
+import { Ionicons } from '@expo/vector-icons';
+import checkType from '../components/functions/checkType';
+
 const PokedexMove = ({ results, fontSize }) => {
   function Capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -13,28 +16,33 @@ const PokedexMove = ({ results, fontSize }) => {
 
   return (
     <View style={styles.container}>
-      <Text allowFontScaling={false} style={[styles.name, {fontSize: fontSize}]}>{Capitalize(results.identifier.replace('-', ' '))}  </Text>
-      <Text allowFontScaling={false} style={styles.attributes}> Power: {checkForNull(results.power)} | PP: {checkForNull(results.pp)} | Acc: {checkForNull(results.accuracy)}</Text>
+      <View style={{flexDirection: 'column', paddingHorizontal: 12, paddingVertical: 4}}>
+        <Text allowFontScaling={false} style={[styles.name, {fontSize: fontSize, color: checkType(results.type_id)}]}>{Capitalize(results.identifier.replace('-', ' '))}</Text>
+        <Text allowFontScaling={false} style={styles.attributes}>Power: {checkForNull(results.power)} | PP: {checkForNull(results.pp)} | Acc: {checkForNull(results.accuracy)}</Text>
+      </View>
+      <Ionicons style={{ paddingHorizontal: 12 }} name="ios-chevron-forward-sharp" size={18} color="rgb(175, 175, 175)" />  
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 8,
-    marginHorizontal: 12,
-    alignItems: 'baseline',
-    flexDirection: 'column'
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
   },
   name: {
     color: '#fff',
-    fontSize: 25,
     fontWeight: '600',
+    textShadowColor: 'rgb(34, 34, 34)',
+    textShadowOffset: { width: 0, height: 0},
+    textShadowRadius: 3,
   },
   attributes: {
-    paddingVertical: 2,
     color: 'rgb(175, 175, 175)',
     fontSize: 14,
+    paddingLeft: 12,
   },
 });
 
