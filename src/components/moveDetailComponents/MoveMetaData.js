9751dc7b6c87ac5cc08004644b62b46aef089a33
imckain <ianmmckain@gmail.com>
2021-10-19 23:09:03 -0400

    refactored UI, imporoved UX

--- src/components/moveDetailComponents/MoveMetaData.js
+++ src/components/moveDetailComponents/MoveMetaData.js
@@ -11,15 +11,21 @@ const MoveMetaData = ({ results }) => {
   const checkForCollapse = useCallback((el) => {
     if (el === true) {
       return (
-        <Pressable onPressIn={() => setCollapsed(false)}>
-          <Text allowFontScaling={false} style={[styles.metaDataHeaderText]}>Meta Data:  <Entypo name="triangle-right" size={16} color="rgb(175, 175, 175)" /></Text>
-        </Pressable>
+        <View style={styles.headerWrapper}>
+          <Pressable style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}} onPressIn={() => setCollapsed(false)}>
+            <Text allowFontScaling={false} style={[styles.metaDataHeaderText]}>Meta Data:</Text>  
+            <Entypo name="plus" size={16} color="rgb(175, 175, 175)" />
+          </Pressable>
+        </View>
       )
     } if (el === false) {
       return(
-        <Pressable onPressIn={() => setCollapsed(true)}>
-          <Text allowFontScaling={false} style={[styles.metaDataHeaderText]}>Meta Data:  <Entypo name="triangle-down" size={16} color="rgb(175, 175, 175)" /></Text>
-        </Pressable>
+        <View style={styles.headerWrapper}>
+          <Pressable style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}} onPressIn={() => setCollapsed(true)}>
+            <Text allowFontScaling={false} style={[styles.metaDataHeaderText]}>Meta Data:</Text>  
+            <Entypo name="minus" size={16} color="rgb(175, 175, 175)" />
+          </Pressable>
+        </View>
       )
     }
   }, []);
@@ -29,31 +35,33 @@ const MoveMetaData = ({ results }) => {
       return <Text allowFontScaling={false} style={styles.subHeaderText}>Meta Data Not Available</Text>
     }
     else return (
-      <View>
+      <View style={{width: '100%'}}>
         <Text allowFontScaling={false} style={styles.subHeaderText}>Ailment: {el.meta.ailment.name}</Text>
-        {checkForCollapse(collapsed)}
-        <Collapsible collapsed={collapsed}>
-          <View style={styles.scrollViewStyle}>
-            <Text allowFontScaling={false} style={styles.descriptionText}>{el.effect_entries[0].effect.replace('$effect_chance', el.effect_chance)}</Text>
-            <LinearGradient 
-              style={styles.break} 
-              colors={['rgb(223, 223, 223)', 'transparent']} 
-              start={{ x: 0.5, y: 0.5 }}
-              end={{ x: 1, y: 0.5 }}
-            />
-            <Text allowFontScaling={false} style={styles.metaDataText}>Category: {el.meta.category.name}</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Inflict Chance: {el.meta.ailment_chance}</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Crit Rate: {el.meta.crit_rate}</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Drain: {el.meta.drain}</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Flinch Chance: {el.meta.flinch_chance}</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Stat Chance: {el.meta.stat_chance}</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Healing: {el.meta.healing}</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Max Hit: {el.meta.max_hits !== null ? el.meta.max_hits : ' N/A' }</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Max Turns: {el.meta.max_turns !== null ? el.meta.max_turns : ' N/A' }</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Min Hit: {el.meta.min_hits !== null ? el.meta.min_hits : ' N/A' }</Text>
-            <Text allowFontScaling={false} style={styles.metaDataText}>Min Turns: {el.meta.min_turns !== null ? el.meta.min_turns : ' N/A' }</Text>
-          </View>
-        </Collapsible>
+        <View style={styles.metaDataContainer}>
+          {checkForCollapse(collapsed)}
+          <Collapsible collapsed={collapsed}>
+            <View style={styles.scrollViewStyle}>
+              <Text allowFontScaling={false} style={styles.descriptionText}>{el.effect_entries[0].effect.replace('$effect_chance', el.effect_chance)}</Text>
+              <LinearGradient 
+                style={styles.break} 
+                colors={['rgb(223, 223, 223)', 'transparent']} 
+                start={{ x: 0.5, y: 0.5 }}
+                end={{ x: 1.1, y: 0.5 }}
+              />
+              <Text allowFontScaling={false} style={styles.metaDataText}>Category: {el.meta.category.name}</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Inflict Chance: {el.meta.ailment_chance}</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Crit Rate: {el.meta.crit_rate}</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Drain: {el.meta.drain}</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Flinch Chance: {el.meta.flinch_chance}</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Stat Chance: {el.meta.stat_chance}</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Healing: {el.meta.healing}</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Max Hit: {el.meta.max_hits !== null ? el.meta.max_hits : ' N/A' }</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Max Turns: {el.meta.max_turns !== null ? el.meta.max_turns : ' N/A' }</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Min Hit: {el.meta.min_hits !== null ? el.meta.min_hits : ' N/A' }</Text>
+              <Text allowFontScaling={false} style={styles.metaDataText}>Min Turns: {el.meta.min_turns !== null ? el.meta.min_turns : ' N/A' }</Text>
+            </View>
+          </Collapsible>
+        </View>
       </View>
     )
   }
@@ -72,20 +80,31 @@ const MoveMetaData = ({ results }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-    alignItems: 'stretch',
+    alignItems: 'baseline',
     width: '100%',
-    marginHorizontal: 12,
-    marginBottom: 6,
+    borderRadius: 10,
+    backgroundColor: '#464450',
+    paddingVertical: 5,
+    marginBottom: 13,
+    paddingHorizontal: 12,
   },
   subHeaderText: {
     color: '#fff',
     fontWeight: '600',
     fontSize: 22,
   },
+  metaDataContainer: {
+    backgroundColor: '#353340',
+    borderRadius: 10,
+    paddingVertical: 6,
+    marginTop: 6,
+  },
+  headerWrapper: {
+    width: '100%',
+    paddingHorizontal: 12
+  },
   metaDataHeaderText: {
     color: '#fff',
-    marginTop: 6,
-    marginLeft: 12,
     fontWeight: '600',
     fontSize: 18,
   },
@@ -93,16 +112,13 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
     fontWeight: '500',
-    paddingHorizontal: 12,
-    paddingTop: 8,
-    paddingBottom: 6,
-    marginRight: 50,
     fontStyle: 'italic',
+    width: '100%',
+    paddingVertical: 4,
   },
   break: {
     height: 1,
-    width: '80%',
-    marginLeft: 12,
+    width: '100%',
     marginBottom: 8,
     marginTop: 6,
   },
@@ -110,14 +126,13 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'flex-start',
     width: '100%',
-    marginLeft: 12,
+    paddingHorizontal: 24,
   },
   metaDataText: {
     color: 'rgb(223, 223, 223)',
     fontSize: 14,
     fontWeight: '600',
     textAlign: 'center',
-    paddingHorizontal: 12,
     paddingVertical: 3
   },
 });
