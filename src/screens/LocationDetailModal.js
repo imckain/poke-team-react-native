e5e3d3d4f5792197fb2361c7df0b5c1bb7b0e931
imckain <ianmmckain@gmail.com>
2021-10-22 01:08:51 -0400

    added dynamic font sizing to version labels

--- src/screens/LocationDetailModal.js
+++ src/screens/LocationDetailModal.js
@@ -65,12 +65,12 @@ const LocationDetailModal = (props) => {
         keyExtractor={(item) => item.location_area.url}
         renderItem={({ item }) => {
           const version = item.version_details.map(i => {
-            return <Text key={i.version.name} style={styles.versionText}>{i.version.name}</Text>
+            return (i.version.name + ' ')
           })
           return (
             <View style={{paddingBottom: 12}}>
               <View style={styles.versionHeader}>
-                {version}
+                <Text adjustsFontSizeToFit={true}  numberOfLines={2} style={styles.versionText}>{version}</Text>
               </View>
               <View style={styles.locationBox}>
                 <Text style={styles.locationName}>{item.location_area.name.replace('-', ' ')}</Text>
