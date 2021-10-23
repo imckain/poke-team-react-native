1a40e313e61ffdb6ba4aaf71878f5d24ac8c8f88
imckain <ianmmckain@gmail.com>
2021-10-23 03:43:06 -0400

    custom clear button- removes search term keeps api result. added handler to returnnull if no result is populated on load

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { View, StyleSheet, Pressable, Keyboard, TouchableWithoutFeedback, ScrollView } from 'react-native';
 import AdvancedSearchNavigator from '../components/navigatorCards/AdvancedSearchNavigator';
 
+import { Ionicons } from '@expo/vector-icons';
+
 import BuildTeamsNavigator from '../components/navigatorCards/BuildTeamsNavigator';
 import ProfileNavigator from '../components/navigatorCards/ProfileNavigator';
 import SearchBarByName from '../components/navigatorCards/searchBars/SearchBarByName';
@@ -22,18 +24,45 @@ const HomeScreen = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchAPI, results] = useResults([]);
 
+  const showClear = (term) => {
+    if(term !== '') {
+      return (
+        <Pressable 
+          onPress={() => {
+            setSearchTerm('')
+          }} 
+          style={styles.clear}
+        >
+          <Ionicons name="ios-close-circle" size={18} color="rgb(175, 175, 175)" />
+        </Pressable>
+      )
+    } else return null
+  }
+
+  const showResultCard = (el) => {
+    if (el !== null) {
+      return (
+        <Pressable onPress={() => props.navigation.navigate('Detail Modal', { results: el })}>
+          <ShowSearchResult results={el} />
+        </Pressable>
+      )
+    } else return null
+  }
+
   return (
     <HideKeyboard>
       <ScrollView style={styles.container}>
         <StatusBar style='light' />
-        <SearchBarByName 
-          searchTerm={searchTerm} 
-          onSearchTermChange={setSearchTerm} 
-          onSearchTermSubmit={() => searchAPI(searchTerm.replace(' ', '-').toLowerCase())}
-        />
-        <Pressable onPress={() => props.navigation.navigate('Detail Modal', { results: results })}>
-          <ShowSearchResult results={results} />
-        </Pressable>
+        <View style={styles.searchBarContainer}>
+          <SearchBarByName 
+            searchTerm={searchTerm} 
+            onSearchTermChange={setSearchTerm} 
+            onSearchTermSubmit={() => searchAPI(searchTerm.replace(' ', '-').toLowerCase())}
+            style={{zIndex: 0}}
+          />
+          {showClear(searchTerm)}
+        </View>
+        {showResultCard(results)}
         <View style={styles.teamsNavContainer}>
           <Pressable style={styles.buttonStyle} onPress={() => props.navigation.navigate('Teams Tab Nav')} >
             <TeamsNavigator />
@@ -59,6 +88,18 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#353340',
     flex: 1
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 80,
+  },
+  clear: { 
+    zIndex: 1, 
+    position: 'absolute', 
+    alignSelf: 'center',
+    right: 30, 
+    width: 'auto',
   },
   buttonStyle: {
     fontSize: 32,
