29350b7a2dd65da92dae3602a7869ffef780b77c
imckain <ianmmckain@gmail.com>
2021-10-25 23:23:18 -0400

    refactored fi statements to ternarys

--- src/components/functions/checkType.js
+++ src/components/functions/checkType.js
@@ -1,58 +1,41 @@
 export default checkType = (el) => {
-  let backgroundColor = '#353340'
-  if (el === 'normal' || el === 1) {
-    return backgroundColor = '#95965A'
-  }
-  if (el === 'fighting' || el === 2) {
-    return backgroundColor = '#AA240B'
-  }
-  if (el === 'flying' || el === 3) {
-    return backgroundColor = '#977CE4'
-  }
-  if (el === 'poison' || el === 4) {
-    return backgroundColor = '#8A227D'
-  }
-  if (el === 'ground' || el === 5) {
-    return backgroundColor = '#D2AC5C'
-  }
-  if (el === 'rock' || el === 6) {
-    return backgroundColor = '#A88B30'
-  }
-  if (el === 'bug' || el === 7) {
-    return backgroundColor = '#99A91A'
-  }
-  if (el === 'ghost' || el === 8) {
-    return backgroundColor = '#5D4491'
-  }
-  if (el === 'steel' || el === 9) {
-    return backgroundColor = '#A7A7C5'
-  }
-  if (el === 'fire' || el === 10) {
-    return backgroundColor = '#E96C30'
-  }
-  if (el === 'water' || el === 11) {
-    return backgroundColor = '#5683DE'
-  }
-  if (el === 'grass' || el === 12) {
-    return backgroundColor = '#5CC140'
-  }
-  if (el === 'electric' || el === 13) {
-    return backgroundColor = '#F3C723'
-  }
-  if (el === 'psychic' || el === 14) {
-    return backgroundColor = '#FD4174'
-  }
-  if (el === 'ice' || el === 15) {
-    return backgroundColor = '#8BCBD2'
-  }
-  if (el === 'dragon' || el === 16) {
-    return backgroundColor = '#5B2FF0'
-  }
-  if (el === 'dark' || el === 17) {
-    return backgroundColor = '#594735'
-  }
-  if (el === 'fairy' || el === 18) {
-    return backgroundColor = '#DC91DD'
-  }
-  else return backgroundColor
+  return (
+    el === 'normal' || el === 1 ? 
+      '#95965A'
+    : el === 'fighting' || el === 2 ? 
+      '#AA240B'
+    : el === 'flying' || el === 3 ? 
+      '#977CE4'
+    : el === 'poison' || el === 4 ? 
+      '#8A227D'
+    : el === 'ground' || el === 5 ? 
+      '#D2AC5C'
+    : el === 'rock' || el === 6 ? 
+      '#A88B30'
+    : el === 'bug' || el === 7 ? 
+      '#99A91A'
+    : el === 'ghost' || el === 8 ? 
+      '#5D4491'
+    : el === 'steel' || el === 9 ? 
+      '#A7A7C5'
+    : el === 'fire' || el === 10 ? 
+      '#E96C30'
+    : el === 'water' || el === 11 ? 
+      '#5683DE'
+    : el === 'grass' || el === 12 ? 
+      '#5CC140'
+    : el === 'electric' || el === 13 ? 
+      '#F3C723'
+    : el === 'psychic' || el === 14 ? 
+      '#FD4174'
+    : el === 'ice' || el === 15 ? 
+      '#8BCBD2'
+    : el === 'dragon' || el === 16 ? 
+      '#5B2FF0'
+    : el === 'dark' || el === 17 ? 
+      '#594735'
+    : el === 'fairy' || el === 18 ? 
+      '#DC91DD'
+    : '#353340'
+  )
 }
