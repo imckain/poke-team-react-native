462ec86dccda67f8d74537e4faec5fbb2bfd658c
imckain <ianmmckain@gmail.com>
2021-10-27 16:15:23 -0400

    edit team button

---
+++ src/components/buildTeamComponents/EditTeamButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const EditTeamButton = (props) => {
+
+  return (
+    <View style={[styles.container, { height: props.height, width: props.width }]}>
+      <Text allowFontScaling={false} style={styles.label}>Edit</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ff0000',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  label: {
+    fontSize: 30,
+    fontWeight: '600',
+    color: '#fff',
+    textShadowColor: '#53535383',
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 12
+  },
+});
+
+export default EditTeamButton;
