a75fae0c5f0ae34bf0ef56c1c25502c3cb920ad6
imckain <ianmmckain@gmail.com>
2021-11-06 16:07:05 -0400

    UI styling changes

--- src/pokedex/PokedexType.js
+++ src/pokedex/PokedexType.js
@@ -19,8 +19,11 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'baseline',
-    width: '100%'
+    alignItems: 'center',
+    width: '100%',
+    borderBottomColor: '#ffffff18',
+    borderBottomWidth: 1,
+    paddingLeft: 12
   },
   typeBox: {
     paddingVertical: 3,
