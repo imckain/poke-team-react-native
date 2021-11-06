af8695d63a7a9cc921a039a2621f292a0c810ab0
imckain <ianmmckain@gmail.com>
2021-11-05 23:06:17 -0400

    team builder working, context data is being saved through asyncstorage

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,46 +1,52 @@
 import React, { useCallback, useEffect, useReducer } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value)
+    await AsyncStorage.setItem('@storage_Key', jsonValue)
+  } catch (e) {
+    // saving error
+  }
+}
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@storage_Key')
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch(e) {
+    // error reading value
+  }
+}
+
 export default (reducer, actions, initialState) => {
   const Context = React.createContext();
 
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-
-    // console.log(state);
-
-    const storeData = async (value) => {
-      // console.log(value);
-      try {
-        const jsonValue = JSON.stringify(value)
-        await AsyncStorage.setItem('@storage_Key', jsonValue)
-      } catch (e) {
-        // saving error
-        console.log(e);
+    useEffect(() => {
+      async function rehydrate() {
+        const storedState = await getData()
+        dispatch({ type: "hydrate", payload: storedState });
       }
-    }
-    storeData(state)
+      
+      rehydrate()
+    }, []);
+  
+    useEffect(() => {
+      storeData(state);
+    }, [state])
 
-    const getData = async () => {
-      try {
-        const jsonValue = await AsyncStorage.getItem('@storage_Key')
-        return jsonValue != null ? JSON.parse(jsonValue) : null;
-      } catch(e) {
-        // error reading value
-        console.log(e);
-      }
-    }
-
-    const storedState = async() => await getData()
-    console.log(storedState());
-
-    const boundActions ={};
+    console.log(state);
+  
+    const boundActions = {};
     for (let key in actions) {
       boundActions[key] = actions[key](dispatch)
     }
-
-    return <Context.Provider value={{ state: storedState(), ...boundActions }}>{children}</Context.Provider>
+  
+    return <Context.Provider value={{ state, ...boundActions }}>{children}</Context.Provider>
   };
 
   return { Context, Provider };
 };
+
