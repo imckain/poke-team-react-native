56bb8ced5ae15c7217bc4fd6b59d8505b5e561b3
imckain <ianmmckain@gmail.com>
2021-11-06 16:06:39 -0400

    UI styling changes

--- src/components/advancedSearchScreens/TypeSearchScreen.js
+++ src/components/advancedSearchScreens/TypeSearchScreen.js
@@ -73,7 +73,7 @@ const TypeSearchScreen = (props) => {
           }} 
           style={styles.clear}
         >
-          <Ionicons name="ios-close-circle" size={18} color="rgb(175, 175, 175)" />
+          <Ionicons name="ios-close-circle" size={18} color="rgba(32, 32, 32, 0.637)" />
         </TouchableOpacity>
       )
     } else return null
