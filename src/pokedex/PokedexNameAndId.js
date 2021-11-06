863b24f071f6770ef167a579523624b4d01593cb
imckain <ianmmckain@gmail.com>
2021-11-06 16:05:55 -0400

    UI styling changes

--- src/pokedex/PokedexNameAndId.js
+++ src/pokedex/PokedexNameAndId.js
@@ -6,11 +6,13 @@ import { Ionicons } from '@expo/vector-icons';
 const PokedexNameAndId = ({ results, fontSize, numFontSize }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.nameContainer}>
-        <Text style={[styles.name, {fontSize: numFontSize, color: 'rgba(255, 255, 255, 0.5)'}]}>{results.id}</Text>
-        <Text allowFontScaling={false} adjustsFontSizeToFit={true} numberOfLines={1} style={[styles.name, { fontSize: fontSize }]}>{results.identifier.replaceAll('-', ' ')}</Text> 
+      <Text style={[styles.id, { fontSize: numFontSize }]}>{results.id}</Text>
+      <View style={styles.labelContainer}>
+        <View style={styles.nameContainer}>
+          <Text allowFontScaling={false} adjustsFontSizeToFit={true} numberOfLines={1} style={[styles.name, { fontSize: fontSize }]}>{results.identifier.replaceAll('-', ' ')}</Text> 
+        </View>
+        <Ionicons style={{ paddingHorizontal: 12 }} name="ios-chevron-forward-sharp" size={18} color="rgb(175, 175, 175)" />  
       </View>
-      <Ionicons style={{ paddingHorizontal: 12 }} name="ios-chevron-forward-sharp" size={18} color="rgb(175, 175, 175)" />  
     </View>
   );
 };
@@ -22,6 +24,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '100%'
   },
+  labelContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderBottomColor: '#ffffff18',
+    borderBottomWidth: 1,
+    marginLeft: 12,
+    flex: 1,
+  },
   nameContainer: {
     flexDirection: 'row', 
     alignItems: 'center', 
@@ -29,6 +40,12 @@ const styles = StyleSheet.create({
   },
   name: {
     color: '#fff',
+    fontWeight: '600',
+    marginVertical: 6,
+    textTransform: 'capitalize',
+  },
+  id: {
+    color: 'rgba(255, 255, 255, 0.5)',
     fontWeight: '600',
     paddingHorizontal: 12,
     marginVertical: 6,
